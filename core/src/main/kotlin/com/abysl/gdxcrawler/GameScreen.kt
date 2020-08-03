9599f12b80ede295619556724e8170d49a5219ae
Andrew Bueide <andrew@abueide.com>
2020-08-02 18:31:21 -0600

    Comment

--- core/src/main/kotlin/com/abysl/gdxcrawler/GameScreen.kt
+++ core/src/main/kotlin/com/abysl/gdxcrawler/GameScreen.kt
@@ -22,7 +22,6 @@ import kotlin.math.roundToInt
 class GameScreen : Screen {
     val batch: SpriteBatch = SpriteBatch()
     val test: Sprite = Sprite(Texture("default.png"))
-    val viewport: PixelPerfectViewport = PixelPerfectViewport(320f, 180f)
     val camera: OrthographicCamera = OrthographicCamera(20f, 11.25f)
     val assets = AssetManager().also { it.setLoader(TiledMap::class.java, TmxMapLoader()) }
     val map: TiledMap = assets.loadOnDemand<TiledMap>("Overworld.tmx").asset
@@ -47,17 +46,23 @@ class GameScreen : Screen {
     }
 
 
+    // 1 size is 16 tiles
     val tileSize = 16.0
+    // when 16:9 resolution, camera shows 20 tiles, otherwise show a bit more or less of the world
     val baseWidth = 20.0
+    // when 16:9 resolution, camera shows 11.25 tiles, otherwise show a bit more or less of the world
     val baseHeight = 11.25
     override fun resize(width: Int, height: Int) {
+        // calculate the scale that gets closest to showing 20.0 x 11.25 tiles on the screen
         val widthScale = (width.toFloat() / (baseWidth * tileSize)).roundToInt().coerceAtLeast(1)
         val heightScale = (height.toFloat() / (baseHeight * tileSize)).roundToInt().coerceAtLeast(1)
         val scale: Int = maxOf(widthScale, heightScale)
+        // scale the tiles by that amount
         val tileFactor: Double = scale.toDouble() * tileSize
-        val widthMod: Double = (width - (baseWidth * tileFactor)) / tileFactor //- 0.01
-        val heightMod: Double = (height.toFloat() - (baseHeight * tileFactor)) / tileFactor // - 0.01
-        println("$width, $height")
+        // calculate the camera width needed with the proper scale to keep things pixel perfect
+        val widthMod: Double = (width.toDouble() - (baseWidth * tileFactor)) / tileFactor
+        // calculate the camera height needed to keep things pixel perfect
+        val heightMod: Double = (height.toDouble() - (baseHeight * tileFactor)) / tileFactor
         camera.viewportWidth = (baseWidth + widthMod).toFloat()
         camera.viewportHeight = (baseHeight + heightMod).toFloat()
     }
